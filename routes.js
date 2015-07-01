048e27900b0f662149ad4205d23cf222e994437b
Sameeri Marryboyina <smarryboyina@telogical.com>
2015-07-01 10:02:17 -0700

    add greeting route, modify routes.js to be generic

--- routes.js
+++ routes.js
@@ -1,6 +1,25 @@
-var home = require('./resources/home/resource.js');
-var debug = require('./resources/debug/resource.js');
+//var home = require('./resources/home/resource.js');
+//var debug = require('./resources/debug/resource.js');
+//var greeting = require('./resources/greeting/resource.js');
+//
+//var routes = [home, debug, greeting];
+//module.exports = routes;
 
-var routes = [home, debug];
+
+
+
+
+var _ = require('lodash');
+
+var utilities = require('./lib/utilities');
+
+var resourceDirs = utilities.getDirectories('./resources');
+
+var routes = _.map(resourceDirs, function(resourceDir){
+    var routePath = './resources/' + resourceDir + '/resource.js';
+    var route = require(routePath);
+    return route;
+});
 
 module.exports = routes;
+
