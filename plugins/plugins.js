cb5f6ebf8ac1c01ced7cff2017b3f573af5ccaeb
sameeri <smarryboyina@telogical.com>
2016-02-10 22:39:49 -0600

    Adding lout :)

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -2,24 +2,12 @@
 
 var inert = require('inert');
 var vision = require('vision');
-var HapiSwagger = require('hapi-swagger');
-
-var hapiSwaggerOptions = {
-    info: {
-            'title': 'API Docs',
-            'version': '1.0',
-        }
-    };
-
-var hapiSwaggerPlugin =  {
-    'register': HapiSwagger,
-    'options': hapiSwaggerOptions
-};
+var lout = require('lout');
 
 var plugins =  [
         inert,
         vision,
-        hapiSwaggerPlugin
+        lout
       ];
 
 module.exports = plugins;
