9917613ab10b0a14b3a0c01fbd7101ebceb93dfc
sameeri <smarryboyina@telogical.com>
2016-03-10 12:39:26 -0600

    remove games resource(#8)

--- resources/games/DELETE/handler.js
+++
@@ -1,5 +0,0 @@
-function handler (request, reply){
-        reply("DELETE a game by id");
-}
-
-module.exports = handler;
--- resources/games/LIST/handler.js
+++
@@ -1,5 +0,0 @@
-function handler (request, reply){
-  reply("GET all games");
-}
-
-module.exports = handler;
